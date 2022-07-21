5ceb4d2daecb2ad337ceb8fcc4df3a9f9257b324
ShiraliObul <xieraili1986@gmail.com>
2022-07-20 22:17:51 -0400

    Challenge_deliverable2

--- queries/Employee_Database_challenge.sql
+++ queries/Employee_Database_challenge.sql
@@ -1,25 +1,64 @@
 -- Deliverable 1: The Number of Retiring Employees by Title 
+--Steps 1-8;
 SELECT e.emp_no,
        e.first_name,
        e.last_name,
 ti.title,
 ti.from_date,
 ti.to_date
-INTO retirement_titles
+--INTO retirement_titles
 FROM employees AS e
 INNER JOIN titles as ti
 ON (e.emp_no = ti.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-ORDER BY emp_no;
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+ORDER BY e.emp_no;
 
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+--Step 9: Retrieve the employee number, first and last name, and title columns from the Retirement Titles table.
+--FROM retirement_titles AS rt
+--Step 10-14: 
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+       rt.first_name,
+       rt.last_name,
+       rt.title
+       --rt.to_date
+INTO unique_titels
+FROM retirement_titles AS rt
+WHERE rt.to_date ='1999-01-01'
+ORDER BY rt.emp_no, rt.title DESC;
+SELECT * FROM unique_titels;
+--Step 16 retrieve the number of employees by their most recent job title who are about to retire
+SELECT COUNT(ut.emp_no),
+             ut.title
+INTO retiring_titles
+FROM unique_titels AS ut
+GROUP BY ut.title
+ORDER BY COUNT(title) DESC;
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+SELECT * FROM retiring_titles;
+
+--Deliverable 2 The Employees Eligible for the Mentorship Program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+       e.first_name,
+       e.last_name,
+       e.birth_date,
+       de.from_date,
+       de.to_date,
+       ti.title
+INTO mentorship_eligibles
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01') AND
+      (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+SELECT * FROM mentorship_eligibles;
+
+
+
+
+       
